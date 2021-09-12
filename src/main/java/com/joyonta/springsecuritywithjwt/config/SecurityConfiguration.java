dc46e8db87918473385249035fcc11928471f152
Joyonta Saha <joyontasaha8@gmail.com>
2021-09-12 20:22:35 +0600

    authenticate api permit all cofigured

--- src/main/java/com/joyonta/springsecuritywithjwt/config/SecurityConfiguration.java
+++ src/main/java/com/joyonta/springsecuritywithjwt/config/SecurityConfiguration.java
@@ -4,8 +4,10 @@ import com.joyonta.springsecuritywithjwt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -25,5 +27,32 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    /**
+     *  It turned out that antMatcher was working as expected
+     *  & allowing all URLs as intended, but the reason for
+     *  the forbidden response that was getting for the POST APIs was that
+     *  Spring security was waiting for csrf token for
+     *  these POST requests because CSRF protection is enabled
+     *  by default in spring security.
+     *
+     * So in order to make it work , must provide the csrf token in POST request
+     * OR temporarily turn CSRF protection off
+     * (but should enable it again before going to production as this is a serious attack)
+     *
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                // disabling csrf here, you should enable it before using in production
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/authenticate")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
     }
 }
