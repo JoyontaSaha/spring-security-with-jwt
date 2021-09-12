f9c95f2d7d926e185f17a74141a44e00492204ab
Joyonta Saha <joyontasaha8@gmail.com>
2021-09-12 19:56:17 +0600

    authenticationManagerBean method overided in SecurityConfiguration.class

--- src/main/java/com/joyonta/springsecuritywithjwt/config/SecurityConfiguration.java
+++ src/main/java/com/joyonta/springsecuritywithjwt/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.joyonta.springsecuritywithjwt.config;
 import com.joyonta.springsecuritywithjwt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,5 +18,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService);
+    }
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
